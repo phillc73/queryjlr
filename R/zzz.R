7f6bfb824502b67415d730f81f7cb80a5f47ba99
Phill Clarke <phill@starkingdom.co.uk>
2021-03-08 21:28:47 +0100

    Try to set JULIA_HOME interactively

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 
 .onLoad <- function(libname, pkgname) {
 
+    julia_home <- readline(prompt="Please enter full path to the Julia executable on your machine: ")
+    
+    JuliaCall::julia_setup(JULIA_HOME=julia_home)
+
     JuliaCall::julia_install_package_if_needed("Query")
     JuliaCall::julia_install_package_if_needed("DataFrames")
     JuliaCall::julia_library("Query")
