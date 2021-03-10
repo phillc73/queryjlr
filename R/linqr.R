d06c30468c75b21b52489e59c262acfc47b5e981
Phill Clarke <phill@starkingdom.co.uk>
2021-03-10 23:49:39 +0100

    Backing out tracemem() until it works

--- R/linqr.R
+++ R/linqr.R
@@ -2,7 +2,6 @@ linqr <- function(query) {
 
     query_trim <- stringi::stri_trim_both(query)
     
-    # if there isn't a named dataframe, string queries
     # Extract dataframe name using string splits
     query_split <- stringi::stri_split_fixed(query_trim, "in",
                                          omit_empty = TRUE,
@@ -13,51 +12,16 @@ linqr <- function(query) {
     from_df <- stringi::stri_extract_first_words(query_split[2], 
                                                    locale = NULL)
 
-    # tracemem for the named dataframe
-    memtrace_df <- tracemem(get(from_df))
+    # define the function and setup the JuliaCall command
+    JuliaCall::julia_command(paste0("
+    function queryjl(", from_df, ")",
+    query_trim,
+    " end"))
 
-    # first check if the memtrace list exists
-    if (exists("mem_list") == TRUE){
-
-        # if the list already exists, is the current dataframe included
-        if(memtrace_df %in% mem_list){
-
-            # If the current dataframe is in the memtrace list, 
-            # just execute the query. Fast!!
-            JuliaCall::julia_call("queryjl", julia_object)
-
-        } else {
-
-            # if the current dataframe isn't in the list, add it
-            mem_list[[paste(from_df)]] <- memtrace_df
-
-            # setup the Julia object conversion
-            julia_object <- JuliaCall::JuliaObject(get(from_df))
-            
-            # Execute the query
-            JuliaCall::julia_call("queryjl", julia_object)
-        }
-
-    } else {
-
-        # if the list doesn't exist, create an empty list
-        mem_list <- vector(mode = "list")
-
-        # add current dataframe to newly created empty list
-        mem_list[[paste(from_df)]] <- memtrace_df
-
-        # define the function and setup the JuliaCall command
-        JuliaCall::julia_command(paste0("
-        function queryjl(", from_df, ")",
-        query_trim,
-        " end"))
-
-        # setup the Julia object conversion
-        julia_object <- JuliaCall::JuliaObject(get(from_df))
-            
-        # Execute the query
-        JuliaCall::julia_call("queryjl", julia_object)
-
-    }
+    # setup the Julia object conversion
+    julia_object <- JuliaCall::JuliaObject(get(from_df))
+        
+    # Execute the query
+    JuliaCall::julia_call("queryjl", julia_object)
 
 }
